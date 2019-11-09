60aa0a48bff708eb98868f3c1b463ce546179208
yanch <8232205+yaaanch@users.noreply.github.com>
2019-11-09 23:48:39 +0800

    Redoes the changes, but fixed this time

--- src/main/java/seedu/address/commons/core/GuiTheme.java
+++ src/main/java/seedu/address/commons/core/GuiTheme.java
@@ -5,7 +5,8 @@ package seedu.address.commons.core;
  */
 public enum GuiTheme {
     LIGHT("light", "view/LightTheme.css"),
-    DARK("dark", "view/DarkTheme.css");
+    DARK("dark", "view/DarkTheme.css"),
+    HACKER("hacker", "view/HackerTheme.css");
 
     public static final String MESSAGE_CONSTRAINTS = "This mode is not valid";
 
